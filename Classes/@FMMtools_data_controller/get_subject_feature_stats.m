be55d1e01cd7a3bb6c70bd6be5114f2f115264cb
yalexand <yalexand@imperial.ac.uk>
2016-07-25 16:18:15 +0100

    fixed the "prcntg_anno_time_latent_also_latent_by_sensor" - by undoing preliminary dilation of segmented ROIs

--- Classes/@FMMtools_data_controller/get_subject_feature_stats.m
+++ Classes/@FMMtools_data_controller/get_subject_feature_stats.m
@@ -100,18 +100,14 @@ if isempty(obj.ADC_trails_features_data), return, end;
                     v3 = quantile(sample,.25);
                     v4 = median(sample);
                     v5 = quantile(sample,.75);
-                params_data = [params_data v1 v2 v3 v4 v5 ];                    
-                %
+                params_data = [params_data v1 v2 v3 v4 v5 ];                                    
                 % STATS ON LATENCY INTERVALS - ENDS                                                                
                 %
-                params_data = [params_data num2cell(tot_annotators_time)];
-                params_data = [params_data num2cell(tot_annotators_projected_ROIs)];
-                %
                 % start %%%%%%% the % of the latent annotated time, also latent according to the detection
-                dil_SGM = imdilate(SGM,strel('line',round(2.5*obj.Fs_ADC),90));
+                % dil_SGM = imdilate(SGM,strel('line',round(2.5*obj.Fs_ADC),90)); % not sure if dilation is needed
+                dil_SGM = SGM;
                 prcntg_anno_time_latent_also_latent_by_sensor = sum( (~anno_tot) & (~dil_SGM) )/sum( ~anno_tot);
                 % end %%%%%%% the % of the latent annotated time, also latent according to the detection                                                
-                params_data = [params_data num2cell(prcntg_anno_time_latent_also_latent_by_sensor)];
                 %                    
                 % number of active channels
                 num_ADC_channels = size(obj.current_data.ADC,2);
@@ -126,8 +122,6 @@ if isempty(obj.ADC_trails_features_data), return, end;
                     end                                            
                 end                                                
                 % number of active channels
-                params_data = [params_data num2cell(n_active_channels)];
-                params_data = [params_data num2cell(n_used_channels)];
                 %
                 % estimated "number of annotations out of annotations"
                 % two ways of doing that
@@ -136,15 +130,23 @@ if isempty(obj.ADC_trails_features_data), return, end;
                 % 
                 z_lab = bwlabel(imdilate(SGM &~ anno_tot,strel('line',round(2.5*obj.Fs_ADC),90))); % dilate and count
                 %disp([max(z_lab) num_anno_out]);
-                params_data = [params_data num2cell(max(z_lab))]; % num_anno_out               
-                                
+                num_anno_out = max(z_lab);
+
+                params_data = [params_data num2cell(prcntg_anno_time_latent_also_latent_by_sensor)];
+                params_data = [params_data num2cell(n_active_channels)];
+                params_data = [params_data num2cell(n_used_channels)];
+                params_data = [params_data num2cell(tot_annotators_time)];
+                params_data = [params_data num2cell(tot_annotators_projected_ROIs)];
+                params_data = [params_data num2cell(num_anno_out)];                
+
                 stats = [stats; params_data];
             end
             if ~isempty(hw), delete(hw), drawnow; end;            
             
             names = [names {'mean_ltntT','std_ltntT','Q25_ltntT','median_ltntT','Q75_ltntT', ...
-                'tot_anno_time','num_anno_proj_ROIs','prcntg_anno_time_latent_also_latent_by_sensor', ...
-                'n_actv_dtctrs','n_used_dtctrs','num_anno_out'}];            
+                'prcntg_anno_time_latent_also_latent_by_sensor', ...
+                'n_actv_dtctrs','n_used_dtctrs', ...
+                'tot_anno_time','num_anno_proj_ROIs','num_anno_out'}];            
             
             stats = [names; stats];
 end
